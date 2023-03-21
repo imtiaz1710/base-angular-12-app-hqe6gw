7b7b585717aceab291f5cacfc1fd24d2964e93a4
imtiaz1710 <32028840+imtiaz1710@users.noreply.github.com>
2023-03-21 15:25:51 +0600

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,110 +1,76 @@
-import { Component, NgZone, AfterViewInit } from '@angular/core';
+import { Component, Inject, NgZone, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
+// amCharts imports
 import * as am4core from '@amcharts/amcharts4/core';
-import * as am4maps from '@amcharts/amcharts4/maps';
-import am4geodata_worldHigh from '@amcharts/amcharts4-geodata/worldHigh';
+import * as am4charts from '@amcharts/amcharts4/charts';
+import am4themes_animated from '@amcharts/amcharts4/themes/animated';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+export class AppComponent {
+  private chart: am4charts.XYChart;
 
-export class AppComponent implements AfterViewInit {
-  constructor(private zone: NgZone) {
+  constructor(@Inject(PLATFORM_ID) private platformId, private zone: NgZone) {}
 
-  }
-
-    ngAfterViewInit() {
-    
-/* Create map instance */
-var chart = am4core.create('chartdiv', am4maps.MapChart);
-
-/* Set map definition */
-chart.geodata = am4geodata_worldLow;
-
-/* Set projection */
-chart.projection = new am4maps.projections.Miller();
-
-/* Create map polygon series */
-var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
-
-/* Make map load polygon (like country names) data from GeoJSON */
-polygonSeries.useGeodata = true;
-
-/* Configure series */
-var polygonTemplate = polygonSeries.mapPolygons.template;
-polygonTemplate.tooltipText = '{name}';
-polygonTemplate.fill = am4core.color('#74B266');
-polygonTemplate.events.on('hit', function(ev) {
-  ev.target.series.chart.zoomToMapObject(ev.target);
-});
-
-/* Create hover state and set alternative fill color */
-var hs = polygonTemplate.states.create('hover');
-hs.properties.fill = am4core.color('#367B25');
-
-// No thanks, Antarctica, no thanks.
-polygonSeries.exclude = ['AQ'];
-
-chart.zoomControl = new am4maps.ZoomControl();
-
-var buttonTopPosition;
-
-polygonSeries.events.on('inited', () => {
-  polygonSeries.mapPolygons.each(polygon => {
-    const id = polygon.dataItem.dataContext.id;
-    let assignButton;
-
-    switch (id) {
-      case 'US':
-        assignButton = 'us';
-        break;
-
-      case 'BR':
-        assignButton = 'br';
-        break;
-
-      default:
-        break;
-    }
-
-    if (assignButton) {
-      const button = document.getElementById(`${assignButton}-button`);
-      button.addEventListener('click', () => {
-        chart = createMarkers(chart);
-        setTimeout(() => {
-          const animation = chart.zoomToMapObject(polygon);
-          animation.events.on('animationended', () => {
-            chart.homeGeoPoint = chart.centerGeoPoint;
-            chart.homeZoomLevel = chart.zoomLevel;
-          });
-        }, 500);
+  // Run the function only in the browser
+  browserOnly(f: () => void) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.zone.runOutsideAngular(() => {
+        f();
       });
     }
-  });
-});
-    
-    }
+  }
+
+  ngAfterViewInit() {
+    // Chart code goes in here
+    this.browserOnly(() => {
+      am4core.useTheme(am4themes_animated);
+
+      let chart = am4core.create("chartdiv", am4charts.XYChart);
+
+      chart.paddingRight = 20;
+
+      let data = [];
+      let visits = 10;
+      for (let i = 1; i < 366; i++) {
+        visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
+        data.push({ date: new Date(2018, 0, i), name: "name" + i, value: visits });
+      }
+
+      chart.data = data;
+
+      let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+      dateAxis.renderer.grid.template.location = 0;
+
+      let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+      valueAxis.tooltip.disabled = true;
+      valueAxis.renderer.minWidth = 35;
+
+      let series = chart.series.push(new am4charts.LineSeries());
+      series.dataFields.dateX = "date";
+      series.dataFields.valueY = "value";
+      series.tooltipText = "{valueY.value}";
+
+      chart.cursor = new am4charts.XYCursor();
+
+      let scrollbarX = new am4charts.XYChartScrollbar();
+      scrollbarX.series.push(series);
+      chart.scrollbarX = scrollbarX;
+
+      this.chart = chart;
+    });
+  }
+
+  ngOnDestroy() {
+    // Clean up chart when the component is removed
+    this.browserOnly(() => {
+      if (this.chart) {
+        this.chart.dispose();
+      }
+    });
+  }
 }
-
-function createMarkers(chart) {
-  console.log('calling createMarkers');
-  const demoAddress = { my_lat: 35.6895, my_lng: 139.6917 };
-  const mapImageSeries = chart.series.push(new am4maps.MapImageSeries());
-
-  const imageSeriesTemplate = mapImageSeries.mapImages.template;
-  const circle = imageSeriesTemplate.createChild(am4core.Circle);
-  circle.radius = 10;
-  circle.fill = am4core.color('#ff0000');
-  circle.stroke = am4core.color('#FFFFFF');
-  circle.strokeWidth = 2;
-  circle.nonScaling = true;
-  circle.tooltipText = 'hi';
-  imageSeriesTemplate.propertyFields.latitude = 'latitude';
-  imageSeriesTemplate.propertyFields.longitude = 'longitude';
-  mapImageSeries.data = { latitude: demoAddress.latitude, longitude: demoAddress.longitude };
-  return chart;
-}
-
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-    
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
-
-
+export class AppModule {}
